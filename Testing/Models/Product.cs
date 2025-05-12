dabf18951eb1a68a9846862e7488a2ac43851959
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-05-12 01:08:15 -0700

    added an interface

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -2,5 +2,10 @@
 
 public class Product
 {
-    
+    public int ProductID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int CategoryID { get; set; }
+    public int OnSale { get; set; }
+    public int StockLevel { get; set; }
 }
