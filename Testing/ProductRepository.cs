511ee06d67a37c35761ca7c8d235ae788f86fe5b
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-05-12 20:35:07 -0700

    added a productrepo class

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Testing;
+
+public class ProductRepository
+{
+    private readonly IDbConnection _conn;
+
+    public ProductRepository(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+}
